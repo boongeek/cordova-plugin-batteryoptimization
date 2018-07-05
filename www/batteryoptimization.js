fbbc616e8a81064cbdd88c547b57eca62e2a4202
Oleg Kunytskiy <boongeek1970@gmail.com>
2018-07-05 21:50:42 +0300

    Update batteryoptimization.js

--- www/batteryoptimization.js
+++ www/batteryoptimization.js
@@ -1,1 +1,15 @@
-# notes
+var BatteryOptimization = function() {};
+
+/**
+ * Acquire a new wake-lock (keep device awake)
+ *
+ * @param successCallback function to be called when the wake-lock was acquired successfully
+ * @param errorCallback function to be called when there was a problem with acquiring the wake-lock
+ */
+BatteryOptimization.prototype.show = function(successCallback,failureCallback) {
+  
+
+    cordova.exec(successCallback, failureCallback, 'BatteryOptimization', 'show', []);
+}
+
+module.exports = new BatteryOptimization();
